f4bb6865652fdd4967efb1e340f71148b749e1b0
Alex Neustein <alex.neustein@gmail.com>
2018-06-21 18:00:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 
 def find_the_cheese(list_of_items)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  #cheese_types.each do |cheese|
+  cheese_types.each do |cheese|
     list_of_items.find do |items|
   #end
 end
